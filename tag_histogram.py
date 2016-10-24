f4d52121495dbd2462fa62e8131bc7d1f9249fba
bhadkamkar <siddharth.bhadkamkar@gmail.com>
2016-10-24 03:37:12 +0000

    tags4

--- tag_histogram.py
+++ tag_histogram.py
@@ -2,6 +2,9 @@ from question_info_parser import *
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from numpy.random import normal
+
+
 
 matplotlib.use('Agg')
 
@@ -11,11 +14,17 @@ df = load_all_question_info_data()
 x = np.array(df.loc[:]["q_tags"])
 #print x.shape
 #hist, bins = np.histogram(x, bins=50)
-hist = np.histogram(x,bins = 50)
-plt.hist(hist)
+#hist = np.histogram(x,bins = 50)
+gaussian_numbers = normal(size=1000)
+plt.hist(gaussian_numbers)
 plt.title("question tag histogram")
 plt.xlabel("tag bucket")
 plt.ylabel('bucket count')
 plt.figure()
 #plot_url = py.plot_mpl(fig, filename='mpl-basic-histogram')
-plt.savefig("tags_histogram2")
+plt.savefig("tags_histogram3")
+plt.show()
+print gaussian_numbers.shape
+
+
+
