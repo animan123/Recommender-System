892813ac2d4a589d98db373ee593dd92a7ec4ae3
animan123 <anirudh.1203@yahoo.in>
2016-10-24 04:09:24 +0000

    baseline

--- validation_nolabel_parser.py
+++ validation_nolabel_parser.py
@@ -15,8 +15,3 @@ def load_all_validation_data(size=1.0):
 	print "Time to load data: ", (t_end - t_start)
 	return data
 
-data = load_all_validation_data()
-print data.shape
-
-curr_data = load_validation_data(data, "u_id", ["e85d05b6796c351e6a83cfc85309b023", "b19ebc32f3d1de37e12792edc149993f"])
-print curr_data['q_id']
