264e82b1273e3e3a3f93550fb068d58805a5d521
bhadkamkar <siddharth.bhadkamkar@gmail.com>
2016-10-24 03:18:44 +0000

    tag histogram generator and png

---
+++ tag_histogram.py
@@ -0,0 +1,20 @@
+from question_info_parser import *
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+
+matplotlib.use('Agg')
+
+df = load_all_question_info_data()
+
+#x = df.as_matrix(columns=["q_tags"])
+x = np.array(df.loc[:]["q_tags"])
+#print x.shape
+#hist, bins = np.histogram(x, bins=50)
+plt.hist(x)
+plt.title("question tag histogram")
+plt.xlabel("tag bucket")
+plt.ylabel('bucket count')
+plt.figure()
+#plot_url = py.plot_mpl(fig, filename='mpl-basic-histogram')
+plt.savefig("tags_histogram")
