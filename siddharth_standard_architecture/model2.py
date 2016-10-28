28178e83e17904d1b2a6c9ae4b7729f58dc5d2b0
root <root@recommender-system.c.inbound-avatar-142104.internal>
2016-10-28 11:28:46 +0000

    model2 changes

--- siddharth_standard_architecture/model2.py
+++ siddharth_standard_architecture/model2.py
@@ -9,7 +9,8 @@ total_q_tags = 20
 
 X1 = np.load("positiveData")
 X0 = np.load("negativeData")
-
+UTG1 = np.load("positiveUTG")
+UTG0 = np.load("negativeUTG")
 
 X_train = []
 X_test = []
@@ -60,8 +61,40 @@ negative_grp_count_best_tag = []
 for i in range(0,total_q_tags):
 	x = X0[i]
 	negative_grp_count_best_tag.append(np.array(x.shape[0] - np.sum(x[:,best_e_tags_in_positive[i]]))/x.shape[0])
-plt.plot(negative_grp_count_best_tag,'b')
-
+plt.plot(negative_grp_count_best_tag,'b',label = 'ans = 0 when best tag not present')
 fig1 = plt.gcf()
 fig1.savefig("plots/negative_grp")
 plt.clf()
+
+print UTG1[2].shape
+
+for i in range(0,total_q_tags):
+	plt.plot(UTG1[i][:,0])
+	fig1 = plt.gcf()
+	fig1.savefig("upvotes/positive_examples/"+str(i))
+	plt.clf()
+
+	plt.plot(UTG1[i][:,1])
+	fig1 = plt.gcf()
+	fig1.savefig("total_answers/positive_examples/"+str(i))
+	plt.clf()
+
+	plt.plot(UTG1[i][:,2])
+	fig1 = plt.gcf()
+	fig1.savefig("good_answers/positive_examples/"+str(i))
+	plt.clf()
+
+	plt.plot(UTG0[i][:,0])
+	fig1 = plt.gcf()
+	fig1.savefig("upvotes/negative_examples/"+str(i))
+	plt.clf()
+
+	plt.plot(UTG0[i][:,1])
+	fig1 = plt.gcf()
+	fig1.savefig("total_answers/negative_examples/"+str(i))
+	plt.clf()
+
+	plt.plot(UTG0[i][:,2])
+	fig1 = plt.gcf()
+	fig1.savefig("good_answers/negative_examples/"+str(i))
+	plt.clf()
