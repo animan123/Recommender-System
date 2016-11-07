e42804e13fa38c27bb3ed35763179ba4896f6abc
root <root@recommender-system.c.inbound-avatar-142104.internal>
2016-11-07 22:13:56 +0000

    word model complete

--- siddharth_standard_architecture/words_model/word_model.py
+++ siddharth_standard_architecture/words_model/word_model.py
@@ -1,11 +1,14 @@
+import csv
+import pickle
+import time
 import numpy as np
 import pandas as pd
 import json
-
-s = "/"
-print s.split('/')
+from sklearn.model_selection import cross_val_score
+from sklearn import linear_model
 
 """
+
 user_ques_ans_or_not = {}
 
 invited_info_data = pd.read_csv(
@@ -71,7 +74,7 @@ for i in range(0, question_info_data.shape[0]):
 
 json.dump(ques_word_count,open("ques_word_count",'w'))
 
-"""
+
 
 user_answered_word_counts = {}
 user_not_answered_word_counts = {}
@@ -113,6 +116,182 @@ for e_id in user_not_answered_word_counts:
 
 json.dump(user_not_answered_word_counts,open("user_not_answered_word_counts",'w'))
 
-print user_not_answered_word_counts["418f52ed3a2d2b3814b131e75653ba0f"]
 
 
+with open('user_not_answered_word_counts') as data_file:
+        user_not_answered_word_counts = json.load(data_file)
+
+with open('user_answered_word_counts') as data_file:
+        user_answered_word_counts = json.load(data_file)
+
+
+user_word_counts_percent = {}
+for e_id in user_not_answered_word_counts:
+	e_id = str(e_id)
+	na = np.array(user_not_answered_word_counts[e_id])
+	if e_id in user_answered_word_counts:
+		a = np.array(user_answered_word_counts[e_id])
+		percent = a.astype(float)/(na+a)
+		percent = np.nan_to_num(percent)
+		user_word_counts_percent[e_id] = percent.tolist()
+	else:
+		percent = 0.0/(na)
+		percent = np.nan_to_num(percent)
+		user_word_counts_percent[e_id] = percent.tolist()
+
+for e_id in user_answered_word_counts:
+	e_id = str(e_id)
+	if user_word_counts_percent.get(e_id) == None:
+		a = np.array(user_answered_word_counts[e_id])
+		percent = a.astype(float)/(a)
+		percent = np.nan_to_num(percent)
+		user_word_counts_percent[e_id] = percent.tolist()
+
+json.dump(user_word_counts_percent,open("user_word_counts_percent",'w'))
+
+
+
+user_features = {}
+user_info_data =  pd.read_csv("../../raw_data/user_info.txt", names = ['e_id','e_tag','w_id','c_id'], delim_whitespace = True)
+for i in range(0, user_info_data.shape[0]):
+        df_row = user_info_data.iloc[i]
+	e_tag_array = [0 for i in range(0,143)]
+        e_id = str(df_row["e_id"])
+        e_tag_seq = str(df_row["e_tag"])
+        split_e_tag_seq = e_tag_seq.split('/')
+        e_tag_list = [int(x) for x in split_e_tag_seq]
+        for val in e_tag_list:
+                e_tag_array[val] = 1
+	user_features[e_id] = e_tag_array
+
+json.dump(user_features,open("user_features",'w'))
+
+"""
+with open('user_features') as data_file:
+        user_features = json.load(data_file)
+
+
+with open('user_ques_ans_or_not') as data_file:
+        user_ques_ans_or_not = json.load(data_file)
+
+with open('user_word_counts_percent') as data_file:
+	user_word_counts_percent = json.load(data_file)
+
+
+"""
+
+start_time = time.time()
+q_id = "1fb3191c9a2e4333b7939fc488db71d1"
+q_X_words = []
+q_Y_words = []
+q_X_e_tags = []
+q_Y_e_tags = []
+
+
+for e_id in user_ques_ans_or_not[q_id]["answered"]:
+        e_id = str(e_id)
+        q_X_e_tags.append(user_features[e_id])
+        q_Y_e_tags.append(1)
+
+for e_id in user_ques_ans_or_not[q_id]["not_answered"]:
+        e_id = str(e_id)
+        q_X_e_tags.append(user_features[e_id])
+        q_Y_e_tags.append(0)
+
+q_X_e_tags = np.array(q_X_e_tags)
+q_Y_e_tags = np.array(q_Y_e_tags)
+clf_e_tags = linear_model.LogisticRegression()
+#scores = cross_val_score(clf_e_tags, q_X_e_tags, q_Y_e_tags, cv=5)
+clf_e_tags.fit(q_X_e_tags, q_Y_e_tags)
+z = clf_e_tags.predict_proba(q_X_e_tags)
+print q_Y_e_tags
+print z[:,1]
+#print np.equal(q_Y_e_tags,np.array(z))
+#print scores
+end_time = time.time()
+print "time: ",end_time - start_time
+
+
+for e_id in user_ques_ans_or_not[q_id]["answered"]:
+	e_id = str(e_id)
+	q_X_words.append(user_word_counts_percent[e_id])
+	q_Y_words.append(1)
+
+for e_id in user_ques_ans_or_not[q_id]["not_answered"]:
+        e_id = str(e_id)
+        q_X_words.append(user_word_counts_percent[e_id])
+        q_Y_words.append(0)
+
+q_X_words = np.array(q_X_words)
+q_Y_words = np.array(q_Y_words)
+clf_words = linear_model.LogisticRegression()
+#scores = cross_val_score(clf_words, q_X_words, q_Y_words, cv=5)
+clf_words.fit(q_X_words, q_Y_words)
+z = clf_words.predict_proba(q_X_words)
+print q_Y_words
+print z[:,1]
+#print np.equal(q_Y_words,np.array(z))
+#print scores
+end_time = time.time()
+print "time: ",end_time - start_time
+
+
+"""
+
+
+"""
+clfs_words = {}
+clfs_e_tags = {}
+count = 0
+total_count = len(user_ques_ans_or_not)
+for q_id in user_ques_ans_or_not:
+	print q_id,count,"out of",total_count
+	count = count+1
+	q_id = str(q_id)
+	q_X_words = []
+	q_Y_words = []
+	if user_ques_ans_or_not[q_id]["answered"] and user_ques_ans_or_not[q_id]["not_answered"]:
+		for e_id in user_ques_ans_or_not[q_id]["answered"]:
+		        e_id = str(e_id)
+        		q_X_words.append(user_word_counts_percent[e_id])
+       			q_Y_words.append(1)
+		for e_id in user_ques_ans_or_not[q_id]["not_answered"]:
+        		e_id = str(e_id)
+        		q_X_words.append(user_word_counts_percent[e_id])
+        		q_Y_words.append(0)
+
+		q_X_words = np.array(q_X_words)
+		q_Y_words = np.array(q_Y_words)
+		clf_words = linear_model.LogisticRegression()
+		#scores = cross_val_score(clf_words, q_X_words, q_Y_words, cv=5)
+		clf_words.fit(q_X_words, q_Y_words)
+		#z = clf_words.predict_proba(q_X_words)
+		#print q_Y_words
+		#print z[:,1]
+		clfs_words[q_id] = clf_words
+with open('clfs_words','w') as handle:
+	pickle.dump(clfs_words,handle)
+
+"""
+with open('clfs_words','r') as handle:
+        b = pickle.load(handle)
+
+validation_nolabel_data =  pd.read_csv("../../raw_data/validate_nolabel.txt", names = ['q_id','e_id', 'label'], sep = ",")
+
+final_ans = []
+for i in range(1,validation_nolabel_data.shape[0]):
+	df = validation_nolabel_data.iloc[i]
+	q_id = str(df["q_id"]) 
+	e_id = str(df["e_id"])
+	z = 0
+	if q_id in b:
+		clf_words = b[q_id]
+		if e_id in user_word_counts_percent:
+			x = np.array(user_word_counts_percent[e_id]).reshape(1,-1)
+			z = clf_words.predict_proba(x)[0][1]
+	f = [q_id,e_id,z]
+	final_ans.append(f)
+
+with open("temp.csv", "w") as fp:
+	a = csv.writer(fp, delimiter=',')
+	a.writerows (final_ans)
